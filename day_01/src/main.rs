3d0358d29c3bcfe87b944a2b781b1effff1f542e
petrSchreiber <petrschreiber@gmail.com>
2018-12-07 01:39:11 +0100

    refactor: solutions for day01

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -6,32 +6,15 @@ use std::{
     io::BufRead
 };
 
-fn part_01(input_file: &str) -> Result<i32, std::io::Error> {
+fn get_increments(input_file: &str) -> Result<Vec<i32>, std::io::Error> {
     let input = match File::open(input_file) {
         Ok(v) => v,
         Err(e) => return Err(e)
     };
-    let buffered = std::io::BufReader::new(input);
 
-    let mut result: i32 = 0;
-
-    for line in buffered.lines() {
-        let line_str = &line.unwrap();
-        result += i32::from_str(line_str).unwrap();
-    }
-
-    Ok(result)
-}
-
-fn part_02(input_file: &str) -> Result<i32, std::io::Error> {
-    let input = match File::open(input_file) {
-        Ok(v) => v,
-        Err(e) => return Err(e)
-    };
     let buffered = std::io::BufReader::new(input);
 
     let mut increments: Vec<i32> = Vec::new();
-    let mut frequencies: Vec<i32> = vec![0];
 
     for line in buffered.lines() {
         let line_str = &line.unwrap();
@@ -39,10 +22,20 @@ fn part_02(input_file: &str) -> Result<i32, std::io::Error> {
         increments.push(increment);
     }
 
+    Ok(increments)
+}
+
+fn get_frequency(increments: &Vec<i32>) -> i32 {
+    increments.into_iter().sum::<i32>()
+}
+
+fn get_repeated_frequency(increments: &Vec<i32>) -> i32 {
+    
+    let mut frequencies: Vec<i32> = vec![0];
     let mut frequency: i32 = 0;
     
     'outer: loop {
-        for increment in &increments {
+        for increment in increments {
             frequency += increment;
 
             if frequencies.contains(&frequency) {
@@ -54,7 +47,7 @@ fn part_02(input_file: &str) -> Result<i32, std::io::Error> {
         }
     }
    
-    Ok(frequency)
+    frequency
 }
 
 fn main() {
@@ -64,23 +57,24 @@ fn main() {
         1 => {
             println!("Please supply input file as parameter");
         },
+
         2 => {
             let input_file = &args[1];
-
-            println!("{}", "-".repeat(79));
-            match part_01(input_file) {
-                Ok(result) => println!("The resulting frequency is {}", result),
-                Err(error) => { println!("Error found while calculating solution for Part 1: {}", error)}
+            let increments = match get_increments(input_file) {
+                Ok(v) => v,
+                Err(error) => {
+                                println!("Error found while retrieving increments from {}:\n{}", input_file, error);
+                                return
+                            }
             };
-            println!("{}", "-".repeat(79));
 
-            println!("{}", "-".repeat(79));
-            match part_02(input_file) {
-                Ok(frequency) => println!("The first frequency my device reaches twice is {}", frequency),
-                Err(error) => { println!("Error found while calculating solution for Part 2: {}", error)}
-            };
-            println!("{}", "-".repeat(79));            
+            let frequency = get_frequency(&increments);
+            println!("The resulting frequency is {}", frequency);
+
+            let repeated_frequency = get_repeated_frequency(&increments);
+            println!("The first frequency my device reaches twice is {}", repeated_frequency);            
         },
+        
         _ => {
             println!("Please supply just 1 parameter: input file");
         }
